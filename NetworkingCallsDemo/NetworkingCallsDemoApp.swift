a5c71dc350e965a1ac6f3313ff1dfc5b42c616d6
Jason Dubon <jasonsdubon@gmail.com>
2024-02-22 15:31:12 -0500

    Initial Commit

---
+++ NetworkingCallsDemo/NetworkingCallsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingCallsDemoApp.swift
+//  NetworkingCallsDemo
+//
+//  Created by Jason Dubon on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingCallsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
